fded1679ee862a761a2812429ee5e0d0287b9f60
Gladwin George <gladwingeorge189@gmail.com>
2024-11-21 19:29:27 +0530

    added navlink scroll navigation animation and adjusted few edits

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//navbar scroll
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.nav-link');
+
+    for (const link of links) {
+        link.addEventListener('click', smoothScroll);
+    }
+
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+});
+
 // home bg animation
 const bgAnimation = document.getElementById('bgAnimation');
 const numberofColorBoxes = 400;
