b7f7da844abc6922db2066511aa8ffa63dd38fda
Gladwin George <gladwingeorge189@gmail.com>
2024-11-29 11:56:51 +0530

    added up button

--- js/script.js
+++ js/script.js
@@ -96,5 +96,26 @@ document.addEventListener('click', function(event) {
 //     loop: true
 // })
 
+//up button
+// Get the button
+var goUpBtn = document.getElementById("go-up-btn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goUpBtn.style.display = "block";
+    } else {
+        goUpBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+goUpBtn.onclick = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
 
